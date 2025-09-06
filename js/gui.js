a1c7cdcc784b8e8853479f4b41d3be0f2cb1ebed
palvert <Pavelver39@gmail.com>
2025-09-06 23:28:21 +0700

    wip: add manage word list #2

--- js/gui.js
+++ js/gui.js
@@ -5,27 +5,29 @@
 
 const word_input = document.querySelector("#word_input");
 const word_table = document.querySelector("#word_table");
-const btn_add_word = document.querySelector("#btn_add_word");
-btn_add_word.addEventListener("click", () => {
-  const new_row = document.createElement("tr");
-  new_row.innerHTML = `
-    <td><div class="div_col_word_type_w"></div></td>
-    <td class="td_col_word">${word_input.value}</td>
-    <td><button class="btn_normal">❌</button></td>
-  `;
-  word_table.appendChild(new_row);
+
+const btn_add_word_black = document.querySelector("#btn_add_word_b");
+btn_add_word_black.addEventListener("click", () => {
+    if (word_input.value) {
+        const new_row = document.createElement("tr");
+        new_row.innerHTML = `
+          <td><div class="div_col_word_type_b"></div></td>
+          <td class="td_col_word">${word_input.value}</td>
+          <td><input class="input_radio" type="checkbox"></input></td>
+        `;
+        word_table.appendChild(new_row);
+    }
 });
 
-
-/*
-    <tr>
+const btn_add_word_white = document.querySelector("#btn_add_word_w");
+btn_add_word_white.addEventListener("click", () => {
+    if (word_input.value) {
+      const new_row = document.createElement("tr");
+      new_row.innerHTML = `
         <td><div class="div_col_word_type_w"></div></td>
-        <td class="td_col_word">Hello</td>
-        <td><button class="btn_normal">❌</button></td>
-    </tr>
-    <tr>
-        <td><div class="div_col_word_type_b"></div></td>
-        <td class="td_col_word">Success</td>
-        <td><button class="btn_normal">❌</button></td>
-    </tr>
-*/
+        <td class="td_col_word">${word_input.value}</td>
+        <td><input class="input_radio" type="checkbox"></input></td>
+      `;
+      word_table.appendChild(new_row);
+    }
+});
